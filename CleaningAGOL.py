d818dd44201103ae6b9399cd4c9f5b9db8899d0d
Cat Schooley <93783235+catschooley@users.noreply.github.com>
2022-01-19 12:21:06 -0700

    Update CleaningAGOL.py

--- CleaningAGOL.py
+++ CleaningAGOL.py
@@ -1,12 +1,19 @@
 from arcgis.gis import GIS
 from arcgis.mapping import WebMap
+import pandas as pd
 
 from getpass import getpass
 portal_url = "https://arcgis.com"
 username = input("Enter AGOL username: ")
+print("Enter password: ")
 password = getpass() # this prompts the user to input a password without echoing, better than hard-coding and storing in the python file
 gis = GIS(portal_url, username, password)
+ownerUsername = input("Please enter username of contents you wish to search: ")
+folderPath = input("Enter file path for csv: ")
+csvName = input("Enter the name for the csv file. DO NOT INCLUDE FILE EXTENSION: ")
+maxItemNumber = int(input("Enter the maximum number of feature service you would like to search. Choose a number less than 10,000: "))
 
+saveLocation = folderPath + csvName + "\\.csv"
 unusedList = []
 
 def main():
@@ -14,11 +21,11 @@ def main():
     print("Logged in as {}".format(gis.properties['user']['username']))
 
     # creates list of items of all map image, feature, vector tile and image services (up to 10000 of each) in active portal
-    services = (gis.content.search(query="owner:" + gis.users.me.username, item_type="Map Service", max_items=10000) +
-                gis.content.search(query="owner:" + gis.users.me.username, item_type="Feature Service", max_items=10000) +
-                gis.content.search(query="owner:" + gis.users.me.username, item_type="Vector Tile Service", max_items=10000) +
-                gis.content.search(query="owner:" + gis.users.me.username, item_type="Image Service", max_items=10000) +
-                gis.content.search(query="owner:" + gis.users.me.username, item_type="Feature Layer", max_items=10000))
+    services = (gis.content.search(query="owner:" + ownerUsername, item_type="Map Service", max_items=maxItemNumber) +
+                gis.content.search(query="owner:" + ownerUsername, item_type="Feature Service", max_items=maxItemNumber) +
+                gis.content.search(query="owner:" + ownerUsername, item_type="Vector Tile Service", max_items=maxItemNumber) +
+                gis.content.search(query="owner:" + ownerUsername, item_type="Image Service", max_items=maxItemNumber) +
+                gis.content.search(query="owner:" + ownerUsername, item_type="Feature Layer", max_items=maxItemNumber))
 
     print(f'Searching webmaps in {portal_url}')
 
@@ -48,15 +55,11 @@ def main():
         print("{} | {}".format(service.title ,portal_url + r'/home/item.html?id=' + service.id))
         unusedList.append([service.title, portal_url + r'/home/item.html?id=' + service.id])
     print("There are a total of {} unused services in your portal".format(str(len(services))))
+    
+    df = pd.DataFrame(unusedList)
+
+    df.columns = ["Name", "AGOL Link"]
+
+    df.to_csv(saveLocation, index = False)
 
 main()
-
-import pandas as pd
-
-df = pd.DataFrame(unusedList)
-
-df.columns = ["Name", "AGOL Link"]
-
-saveFolder = input("Enter file path for csv: ")
-
-df.to_csv(saveFolder, index = False)
